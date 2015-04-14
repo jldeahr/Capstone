17ef715c44bfd8f444475a07f1ca865fe3bc5f46
jldeahr <jldeahr@users.noreply.github.com>
2015-04-14 11:23:03 -0500

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.io.FileNotFoundException;
 
 /**
@@ -12,11 +13,23 @@ import java.io.FileNotFoundException;
 public class Rules
 {
     final static String[] options = {"Random", "Date", "Password Length"};
+	static ArrayList<String> chars = new ArrayList();
+	final static String[] dateOptions = {“dd/mm/yyyy”, “mm/dd/yyyy, “dd-mm-yyyy”, “mm-dd-yyyy”};
+	static ArrayList<String> dates = new ArrayList();
+	GregorianCalendar cal = new Gregorian Calendar();
+
 
     public static void main(String[] args)
     throws FileNotFoundException
     {
-        userInput();
+        ArrayList<String> input = new ArrayList<String>(userInput());
+		for (int i = 0; i < input.size(); i++)
+		{
+			if (input.get(i).compareTo(“Date”))
+			{
+				date();
+			}
+		}
     }
 
     /**
@@ -51,8 +64,78 @@ public class Rules
                 }
             }
         }
-        
+        scanner.close();
         return input;
     }
+    
+    private static String rgen(String str, int count, ArrayList<String> chars)
+    {
+        if (count == 0)
+        {
+            return str;
+        }
+        else
+        {
+            for (int i = o; i < chars.size(); i++)
+            {
+                str +=  (String)chars.get(i);
+                count--;
+                rgen(str,count,chars);
+                System.out.println(str);
+            }
+        }
+    }
 
+    private static void charChoice()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the possible characters (separated by enter): ");
+        while (in.hasNext())
+        {
+            chars.add(in.next());
+        }
+		in.close();
+    }
+
+	private static void date()
+	{
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < dateOptions.length; i++)
+        {
+            System.out.println("\t" + (i+1) + ". " + dateOptions[i]);
+        }
+
+		if (in.next().compareTo(dateOptions[0]))
+		{
+			dates.add((String)day() + “/” + (String)month() + “/” + (String)year());
+		}
+     if (in.next().compareTo(dateOptions[1]))
+		{
+			dates.add((String)month() + “/” + (String)day() + “/” + (String)year());
+		}
+		if (in.next().compareTo(dateOptions[2]))
+		{
+			dates.add((String)day() + “-” + (String)month() + “-” + (String)year());
+		}
+		if (in.next().compareTo(dateOptions[3]))
+		{
+			dates.add((String)month() + “-” + (String)day() + “-” + (String)year());
+		}
+		in.close();
+	}
+	
+	private static String year()
+	{
+		return (String)cal.get(Calendar.YEAR);
+	}
+
+	private static String day()
+	{
+		return (String)cal.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	private static String month()
+	{
+		return (String)cal.get(Calendar.Month);
+	}
 }
